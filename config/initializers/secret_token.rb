b7dad72c3f3bb39587ee9a90d38188542cc7a813
unknown <david_000@SpeedDragon.(none)>
2012-07-24 18:46:05 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bestflix::Application.config.secret_token = 'ea02ab2a348792e0b8648e8a76957d01588ab89ac9d84f2f0c82637b1d1580044370fe5d7725a1c6da4f0aec483b05c0a2606075e2b8fdac60946e7109c3cfb5'
